65f2acaea18664a7f0b756d27ea294bf162659a6
Saradagithu <105852814+Saradagithu@users.noreply.github.com>
2022-07-08 11:38:30 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,7 +38,7 @@ global.APIKeys = {
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
@@ -62,10 +62,10 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.ownertag = '916909137213' //ur owner number
+global.botname = 'Cheems Bot MD V4' //ur owner name
+global.linkz = "https://t.me/+3d20o8e714Y2NjQ1" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.link/icqo7v" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
